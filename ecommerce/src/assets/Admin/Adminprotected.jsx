0e9e4c22b051161eb461d7b89765c332e9809ecd
hafeefak <hafeefak2001@gmail.com>
2025-03-28 05:49:23 +0530

    deleted duplicated folder

--- ecommerce/src/assets/Admin/Adminprotected.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-export default function Adminprotect({children}){
-
-
-        const isadmin=localStorage.getItem("isAdmin")==="true";
-        return  isadmin? children :<Navigate to ="/adminlogin" replace/>
-}   
